fb221e6f79f3152de4bb4c52d6e7493adc04e63f
FaceMan01 <petetskaja@yandex.ru>
2024-09-30 22:42:26 +0500

    update-prolog-4

--- Prolog/3/5.pl
+++ Prolog/3/5.pl
@@ -9,7 +9,7 @@ find_min_max(N, Min, Max) :-
     N2 is N // 10,
     find_min_max(N2, Min1, Max1),
     (
-        N1 < Min1 -> Min = N1, Max = Max1 ;
-        N1 > Max1 -> Min = Min1, Max = N1 ;
+        N1 < Min1 -> Min = N1, Max = Max1;
+        N1 > Max1 -> Min = Min1, Max = N1;
         N1 =< Max1, N1 >= Min1 -> Min = Min1, Max = Max1
     ).
